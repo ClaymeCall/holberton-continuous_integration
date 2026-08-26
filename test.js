86359957f87e411180baee599f89ac3a59a8bafa
Clement Callejon <clement.callejon@tutanota.com>
2026-08-26 20:53:31 +0200

    feat: add test that fails if node < 25

--- test.js
+++ test.js
@@ -2,6 +2,13 @@
 // Set to true to pass, false to fail
 const shouldPass = true;
 
+// Test that fails with Node version < 25
+const nodeVersion = parseInt(process.version.match(/^v(\d+)/)[1]);
+if (nodeVersion < 25) {
+  console.error(`Test failed: Node version ${nodeVersion} is less than 25`);
+  process.exit(1);
+}
+
 if (!shouldPass) {
   console.error("Test failed: shouldPass is false");
   process.exit(1);
